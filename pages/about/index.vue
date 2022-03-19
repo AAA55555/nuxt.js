963e0c0673ac342d5578665e0eefa4c4de8b1cbd
Anton Kondratev <kondratev@elma-bpm.com>
2022-03-19 21:38:47 +0400

    Блок 2. работа с базовыми компонентами. много vuex

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>About page!</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, sapiente.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab labore odio omnis repudiandae totam. Itaque iusto minima quibusdam! At cumque deleniti eligendi neque voluptatem.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <h2>{{ pageTitle }}</h2>
+
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <a href="#" @click.prevent="goTo(user)">{{ user.name }} ({{ user.email }})</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  // async asyncData ({store, error}) {
+  //   try {
+  //     await store.dispatch('users/fetchUsers')
+  //     return {}
+  //   } catch (e) {
+  //     error(e)
+  //   }
+  // },
+  // async fetch({store, error}) {
+  //   try {
+  //     if (store.getters['users/users'].length === 0) {
+  //       await store.dispatch('users/fetchUsers')
+  //     }
+  //   } catch (e) {
+  //     error(e)
+  //   }
+  // },
+  data () {
+    return {
+      pageTitle: 'Users page!'
+    }
+  },
+  computed: {
+    users () {
+      return this.$store.getters['users/users']
+    }
+  },
+  methods: {
+    goTo (user) {
+      this.$router.push('/users/' + user.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
